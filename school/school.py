815ace117141e8c6c3a2a17be0358adb16ea921f
Zeki Bene <zackbene9@gmail.com>
2023-04-29 17:56:44 +0200

    Add student adn tacher features added

--- school/school.py
+++ school/school.py
@@ -1,4 +1,16 @@
 import mysql.connector
+from admin import *
+
+""" Menu """
+def menu() -> None:
+    print("------------------------------------------------")
+    print("\t\t0.Add student")
+    print("\t\t1.Add teacher")
+    print("\t\t2.Remove student")
+    print("\t\t3.Remove teacher")
+    print("\t\t4.Find student")
+    print("\t\t5.Find teacher")
+    print("\t\t6.Exit")
 
 """ MySQL Connection """
 mainDB = mysql.connector.connect(
@@ -10,3 +22,24 @@ mainDB = mysql.connector.connect(
 
 """ MySQL Cursor """
 mainCursor = mainDB.cursor()
+
+if __name__ == "__main__":
+    cmd = ""
+
+    menu()
+
+    while cmd != 6:
+        cmd = int(input("$ "))
+        
+        if cmd == 6:
+            print("------------------------------------------------")
+            break
+        elif cmd == 0:
+            addStudent(mainCursor, mainDB)
+            continue
+        elif cmd == 1:
+            addTeacher(mainCursor, mainDB)
+            continue
+        else:
+            print("Invalid command")
+            continue
