754f0c283d5a9706e0dbcd5b55ded845e284a743
Zeki Bene <zackbene9@gmail.com>
2023-04-30 13:41:08 +0200

    User get ticke functioanilty added

---
+++ parking_system/user_entry.py
@@ -0,0 +1,73 @@
+import mysql.connector
+import tkinter as tk
+import ttkbootstrap as ttk
+import random
+
+""" USER WINDOW CLASS """
+class UserEntryWindow(ttk.Window):
+    def __init__(self, argDB: any, argCursor: any, title="Parking ticker purchase") -> None:
+        super().__init__(title=title, themename="darkly", size=(300, 300), resizable=(False, False))
+
+        #Variables
+        self.argDB = argDB
+        self.argCursor = argCursor
+
+        self.getTicketLbl = ttk.Label(master=self, 
+                                    text="TICKET", 
+                                    font="Calibri 20 bold")
+        self.getTickerBtn = ttk.Button(master=self, 
+                                    text="Get ticket", 
+                                    bootstyle="primary", 
+                                    width=15, 
+                                    command=self.getTicket)
+
+        #Mainloop
+        self.packWidgets()
+        self.mainloop()
+
+    #Get ticket function
+    def getTicket(self) -> None:
+        sql = "INSERT INTO `Tickets` (ticket_date, ticket_time, parking_time, fee, ticket_ID) VALUES (CURRENT_DATE, CURRENT_TIME, %s, %s, %s)"
+        val = (0, 0, self.genTicketID())
+        self.argCursor.execute(sql, val)
+        self.argDB.commit()
+
+    #Generate ticketID
+    def genTicketID(self) -> str:
+        genID = ""
+        abc = "QWERTYUIOPASDFGHJKLZXCVBNM"
+        nums = "123456789"
+
+        for i in range(5):
+            if i == 0:
+                randNum = nums[random.randrange(len(nums))]
+                genID = genID + randNum
+            elif i % 2 == 0:
+                randLetter = abc[random.randrange(len(abc))]
+                genID = genID + randLetter
+            else:
+                randNum = nums[random.randrange(len(nums))]
+                genID = genID + randNum
+
+        return genID
+
+    #Pack function
+    def packWidgets(self) -> None:
+        self.getTicketLbl.pack(pady=50)
+        self.getTickerBtn.pack()
+
+
+""" DATABASE CONNECTION """
+mainDB = mysql.connector.connect(
+    host="b9zwuf9k9awvthq1z5r1-mysql.services.clever-cloud.com",
+    user="ubhyrtpkdvjshhsa",
+    password="Nul8kN9sDTonpg30CZZr",
+    database="b9zwuf9k9awvthq1z5r1"
+)
+
+""" CURSOR """
+mainCursor = mainDB.cursor()
+
+""" MAIN DRIVER """
+if __name__ == "__main__":
+    userWindow = UserEntryWindow(mainDB, mainCursor)
